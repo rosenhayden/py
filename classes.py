f51b5a1fbc6c5d313836866cb12430a374a33818
Hayden Rosen <91492781+rosenhayden@users.noreply.github.com>
2024-05-19 15:41:10 -0700

    Add files via upload

--- classes.py
+++ classes.py
@@ -5,6 +5,9 @@
 
 #instance variables contain data that is unique to each instance of a class
 class Employee:
+    #class vars are the same among all instances.
+    raise_amount = 1.04
+    num_of_employees = 0
     #this initalizes the constructor
     def __init__(self, first, last, pay):
         #sets self(our instace) to the values passed 
@@ -12,9 +15,14 @@ class Employee:
         self.last = last
         self.pay = pay
         self.email = first + "." + last + '@company.com'
+        #since every time we make an employee the init method runs this line will run, keeping count
+        Employee.num_of_employees += 1
     #method to get full name
     def fullname(self):
         return "{} {}".format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
 
 
 #classes are the blueprint that get used by instances of that class
